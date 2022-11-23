a5933fe7ddb1027188dbc94a0e55d7d3dd06fbf4
Arian Hojat <armyofda12mnkeys@gmail.com>
2022-11-23 01:50:41 -0500

    got Connect working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,9 @@ declare global {
 })
 export class AppComponent {
   wallet: ethers.Wallet | undefined;
-  connectedMetaMaskSigner: ethers.providers.JsonRpcSigner | undefined; //MetaMask wallets aren't really Wallets, not sure how to connect to them yet
+  signer: ethers.Signer | undefined;
+  walletAddress: string | undefined;
+
   provider: ethers.providers.BaseProvider | undefined;
   etherBalance: number | undefined = 0;
   tokenContract: ethers.Contract | undefined;
@@ -45,15 +47,10 @@ export class AppComponent {
   async createWallet() {
     this.provider = new ethers.providers.AlchemyProvider("goerli", ALCHEMY_API_KEY); //ethers.getDefaultProvider("goerli", {alchemy: ALCHEMY_API_KEY, etherscan: ETHERSCAN_API_KEY});
     this.wallet = ethers.Wallet.createRandom().connect(this.provider); //
+    this.signer = this.wallet;
+    this.walletAddress = this.wallet.address;
     //console.log("create random wallet, pub key: "+ this.wallet.publicKey); //
     //console.log("create random wallet, priv key: "+ this.wallet.privateKey); //if you want to re-import later
-    /*this.http.get<{result: string}>("http://localhost:3000/token-address").subscribe((ans)=>{//not a Promise, returns Observable 
-      console.log(ans.result);
-      this.tokenContractAddress = ans.result;
-
-      this.updateBlockchainInfo();
-      setInterval( this.updateBlockchainInfo.bind(this), 5000);
-    });*/
     this.tokenContractAddress = ERC20_VOTES_ADDRESS;
     this.ballotContractAddress = BALLOT_CONTRACT_ADDRESS;
     this.tokenContract = new ethers.Contract( ERC20_VOTES_ADDRESS, tokenJsonInterface.abi, this.wallet); //note: for these this.provider works for read-only calls, but write calls like delegate need a real signer
@@ -64,51 +61,26 @@ export class AppComponent {
     this.updateBlockchainInfo();
     setInterval( this.updateBlockchainInfo.bind(this), 5000);
 
-    /*
-    //testing out this method to loop over a solidity array until it goes out-of-bounds error vs adding a length param to the contract
-    const solidityPublicArrayToJsArray = async<T = any>(contract: ethers.Contract, arrayName: string) => {
-      let err: Error| undefined = undefined;
-      let array: T[] = [];
-      while (err === undefined) {
-        try {
-          const item = await contract[arrayName](array.length) as T;
-          array.push(item);
-        } catch (e) {
-          err = e as Error;
-        }
-      }
-      return array;
-    }
-    const arr = await solidityPublicArrayToJsArray(this.tokenContract, "proposals");
-    console.log(arr);
-    */
-    //const proposals = this.tokenContract['proposals']().then((ans:string)=>{
-    //  console.log(ans);
-    //});
-    //const proposal = await this.tokenContract['proposals'][0]; //.name;
-    //console.log(proposal);
-    //console.log( 'proposal#1:'+ ethers.utils.parseBytes32String( proposal) );
   }
-
-  //TODO: not sure how to get this to work
+  
   async connectWallet(){
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     // Prompt user for account connections
     await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
+    const signer = provider.getSigner();    
+    this.signer = signer;
+    this.walletAddress = await this.signer.getAddress();
+    console.log("walletAddress is: "+ this.walletAddress);
     const connected_account_addr = await signer.getAddress();
-    console.log("Account:", connected_account_addr);
-
-    this.connectedMetaMaskSigner = signer;
+    console.log("Account:", connected_account_addr);    
 
     
     this.provider = provider; //ethers.getDefaultProvider("goerli", {alchemy: ALCHEMY_API_KEY, etherscan: ETHERSCAN_API_KEY});
-    //this.wallet = ethers.Wallet.createRandom().connect(this.provider); //
-    //this.wallet = new ethers.Wallet(signer);
+    this.wallet = undefined; //hmmm is there no wallet?
     this.tokenContractAddress = ERC20_VOTES_ADDRESS;
     this.ballotContractAddress = BALLOT_CONTRACT_ADDRESS;
-    this.tokenContract = new ethers.Contract( ERC20_VOTES_ADDRESS, tokenJsonInterface.abi, this.wallet);
-    this.ballotContract = new ethers.Contract( BALLOT_CONTRACT_ADDRESS , tokenizedBallotJsonInterface.abi, this.wallet); //could be hardcoded vs returning from API
+    this.tokenContract = new ethers.Contract( ERC20_VOTES_ADDRESS, tokenJsonInterface.abi, signer);
+    this.ballotContract = new ethers.Contract( BALLOT_CONTRACT_ADDRESS , tokenizedBallotJsonInterface.abi, signer); //could be hardcoded vs returning from API
 
     this.updateBlockchainInfo();
     setInterval( this.updateBlockchainInfo.bind(this), 5000);
@@ -119,15 +91,9 @@ export class AppComponent {
     const wallet = new ethers.Wallet( private_key ?? "");
     const signer = wallet.connect(this.provider);
     this.wallet = signer;
+    this.walletAddress = this.wallet.address;
     //console.log("create random wallet, pub key: "+ this.wallet.publicKey); //
     //console.log("create random wallet, priv key: "+ this.wallet.privateKey); //if you want to re-import later
-    /*this.http.get<{result: string}>("http://localhost:3000/token-address").subscribe((ans)=>{//not a Promise, returns Observable 
-      console.log(ans.result);
-      this.tokenContractAddress = ans.result;
-
-      this.updateBlockchainInfo();
-      setInterval( this.updateBlockchainInfo.bind(this), 5000);
-    });*/
     this.tokenContractAddress = ERC20_VOTES_ADDRESS;
     this.ballotContractAddress = BALLOT_CONTRACT_ADDRESS;
     this.tokenContract = new ethers.Contract( ERC20_VOTES_ADDRESS, tokenJsonInterface.abi, signer); //note: for these this.provider works for read-only calls, but write calls like delegate need a real signer
@@ -138,25 +104,35 @@ export class AppComponent {
   }
   async updateBlockchainInfo() {
     console.log('updateBlockchainInfo');
+    console.log('walletAddress is: '+this.walletAddress);
     //console.log(this.tokenContractAddress);
     //console.log(this.wallet);
-    if( this.tokenContract && this.wallet && this.ballotContract) {
+    let wallet_address: string;
+    if( this.tokenContract && (this.wallet || this.signer) && this.ballotContract) {
       //console.log('inside update');
       //this.tokenContract = new ethers.Contract(this.tokenContractAddress, tokenJsonInterface.abi, this.provider);
-      this.tokenContract["balanceOf"](this.wallet.address).then((tokenBalanceBigNumber:BigNumber)=>{
+      this.tokenContract["balanceOf"](this.walletAddress).then((tokenBalanceBigNumber:BigNumber)=>{
         //console.log( tokenBalanceBigNumber );
         //console.log( ethers.utils.formatEther(tokenBalanceBigNumber) );
         this.tokenBalance = parseFloat( ethers.utils.formatEther(tokenBalanceBigNumber) );
       });
-      this.tokenContract["getVotes"](this.wallet.address).then((votePowerBigNumber:BigNumber)=>{
+      this.tokenContract["getVotes"](this.walletAddress).then((votePowerBigNumber:BigNumber)=>{
         this.votePower = parseFloat( ethers.utils.formatEther(votePowerBigNumber) ); // add 5 votes to cheat if you want initially
       });
-      this.wallet.getBalance().then( (balanceBigNumber) => {
-        this.etherBalance = parseFloat( ethers.utils.formatEther(balanceBigNumber) );
-        //console.log( balanceBigNumber );
-        //console.log( ethers.utils.formatEther(balanceBigNumber) );
-      });
-      this.ballotContract["votingPower"](this.wallet.address).then((votingPowerBalanceBigNumber:BigNumber)=>{
+      if(this.wallet) {
+        this.wallet.getBalance().then( (balanceBigNumber) => {
+          this.etherBalance = parseFloat( ethers.utils.formatEther(balanceBigNumber) );
+          //console.log( balanceBigNumber );
+          //console.log( ethers.utils.formatEther(balanceBigNumber) );
+        });
+      } else if (this.signer) {
+        this.signer.getBalance().then( (balanceBigNumber) => {
+          this.etherBalance = parseFloat( ethers.utils.formatEther(balanceBigNumber) );
+          //console.log( balanceBigNumber );
+          //console.log( ethers.utils.formatEther(balanceBigNumber) );
+        });
+      }
+      this.ballotContract["votingPower"](this.walletAddress).then((votingPowerBalanceBigNumber:BigNumber)=>{
         this.votingPower = parseFloat( ethers.utils.formatEther(votingPowerBalanceBigNumber) );
       });
 
@@ -190,9 +166,9 @@ export class AppComponent {
     //if(this.tokenContractAddress) {
       //this.tokenContract = new ethers.Contract(this.tokenContractAddress, tokenJsonInterface.abi, this.provider);
       if(this.tokenContract && this.wallet) { //hmmm compiler seems to want this
-        console.log("need to delegate to yourself:"+ this.wallet.address )
+        console.log("need to delegate to yourself:"+ this.walletAddress )
         console.log("contract address"+ this.tokenContract.address+ ", this contracts signer: "+ (await this.tokenContract.signer.getAddress()) );
-        this.tokenContract["delegate"]( this.wallet.address ).then(()=>{
+        this.tokenContract["delegate"]( this.walletAddress ).then(()=>{
           console.log('is delegation done?');
         });
       }
@@ -200,7 +176,7 @@ export class AppComponent {
   }
   requestTokens(){
     //pass name id ?
-    this.http.post<any>('http://localhost:3000/request-tokens', {address: this.wallet?.address} ).subscribe( (ans)=>{//not a Promise, returns Observable 
+    this.http.post<any>('http://localhost:3000/request-tokens', {address: this.walletAddress } ).subscribe( (ans)=>{//not a Promise, returns Observable 
       console.log(ans);
       return ans;
     });
@@ -259,22 +235,4 @@ export class AppComponent {
     return "";
   }
   
-
-  /*
-  title = 'my-fe-app';
-  someText = 'SomeText';
-  lastBlockNumber: string | number | undefined = 'loading...';
-  counter: number = 0
-
-  clickMe() {
-    this.counter++;
-  }
-  constructor() {
-    ethers.providers.getDefaultProvider('goerli').getBlock('latest').then(
-      (block) => {
-        this.lastBlockNumber = block.number;
-      }
-    );
-  }
-  */
 }
